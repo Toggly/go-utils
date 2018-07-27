c0397535eda13c28eb19062eec593bd54f359c3c
Ivan Soloviev <ivan.soloviev@nordigy.ru>
2018-07-26 22:41:55 -0700

    GetRealIPAddr has been added

--- utils.go
+++ utils.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"net/http"
+	"strings"
+)
+
+//GetRealIPAddr gets real IP Addr from request's headers
+func GetRealIPAddr(r *http.Request) string {
+	forward := r.Header.Get("X-Forwarded-For")
+	if forward != "" {
+		ips := strings.Split(forward, ", ")
+		// returns the first item is the list
+		// it means that it the first proxies IPAddr
+		return ips[0]
+	}
+	return r.RemoteAddr
+}
